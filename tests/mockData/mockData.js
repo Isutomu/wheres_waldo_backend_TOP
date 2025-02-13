ac7463b02639b950d12c113b86847705d7c16e81
Isutomu <isutomu@gmail.com>
2025-02-13 17:52:02 -0300

    :card_file_box: raw: Add mock data for tests

---
+++ tests/mockData/mockData.js
@@ -0,0 +1,44 @@
+module.exports.photo = {
+  id: "id",
+  url: "url",
+};
+
+module.exports.characters = [
+  {
+    id: "id1",
+    name: "Odlaw",
+    url: "url",
+  },
+  {
+    id: "id2",
+    name: "Woof",
+    url: "url",
+  },
+];
+
+module.exports.charactersInPicture = [
+  {
+    id: "id1",
+    characterId: "id1",
+    positionX: "1",
+    positionY: "1",
+    photoId: "id",
+  },
+  {
+    id: "id2",
+    characterId: "id2",
+    positionX: "2",
+    positionY: "2",
+    photoId: "id",
+  },
+];
+
+module.exports.session = {
+  id: "id",
+  photoId: "id",
+};
+
+module.exports.score = {
+  photoId: "id",
+  username: "isutomu",
+};
