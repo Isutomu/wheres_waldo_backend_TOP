9884c4b4f40e80b45731722fc3712faeb1fd09ca
Isutomu <isutomu@gmail.com>
2025-02-14 16:10:09 -0300

    :card_file_box: raw: Expand mock data
    There was a need for a session that linked to a photo that contained
    only one character.

---
+++ tests/mockData/mockDataForDb.js
@@ -0,0 +1,68 @@
+module.exports.photos = [
+  { id: "id1", url: "url1" },
+  { id: "id2", url: "url2" },
+];
+
+module.exports.characters = [
+  {
+    id: "id1",
+    name: "Odlaw",
+    url: "url",
+  },
+  {
+    id: "id2",
+    name: "Woof",
+    url: "url",
+  },
+  {
+    id: "id3",
+    name: "Wilma",
+    url: "url",
+  },
+];
+
+module.exports.charactersInPicture = [
+  {
+    id: "id1",
+    positionX: "1",
+    positionY: "1",
+    photoId: "id1",
+    characterId: "id1",
+  },
+  {
+    id: "id2",
+    positionX: "2",
+    positionY: "2",
+    photoId: "id1",
+    characterId: "id2",
+  },
+  {
+    id: "id3",
+    positionX: "3",
+    positionY: "3",
+    photoId: "id2",
+    characterId: "id3",
+  },
+];
+
+module.exports.sessions = [
+  {
+    id: "id1",
+    photoId: "id1",
+  },
+  {
+    id: "id2",
+    photoId: "id2",
+  },
+];
+
+module.exports.scores = [
+  {
+    username: "isutomu",
+    photoId: "id1",
+  },
+  {
+    username: "naruto",
+    photoId: "id2",
+  },
+];
