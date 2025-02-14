b9d3700eccc1e7d2e6b226f4c66db4f57c001aba
Isutomu <isutomu@gmail.com>
2025-02-14 16:12:04 -0300

    :test_tube: feat: Make helper function to add entries on db

---
+++ tests/helpers/addMultipleEntries.js
@@ -0,0 +1,31 @@
+// Local Modules
+const prismaClientSelector = require("../../src/config/prismaClientSelector");
+const prisma = prismaClientSelector();
+
+const addEntries = async (data, type) => {
+  let prismaFunction;
+  switch (type) {
+    case "photos":
+      prismaFunction = prisma.photo.create;
+      break;
+    case "characters":
+      prismaFunction = prisma.character.create;
+      break;
+    case "charactersInPicture":
+      prismaFunction = prisma.characterInPicture.create;
+      break;
+    case "sessions":
+      prismaFunction = prisma.sessions.create;
+      break;
+    case "scores":
+      prismaFunction = prisma.score.create;
+      break;
+    default:
+      throw new Error("No such datatype");
+  }
+  for (const entry of data) {
+    await prismaFunction({ data: entry });
+  }
+};
+
+module.exports = addEntries;
