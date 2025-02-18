c048d6fd622718bb9292bf273839131022481c31
Isutomu <isutomu@gmail.com>
2025-02-18 12:19:49 -0300

    :sparkles: feat: Add session logic
    Since sessions here are not used for auth purposes I did not add
    logic for that (like passport).
    They are merely used to identify a user playing the game,
    and we use the same table to keep track of information relevant
    to, again, playing the game.

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@
 require("dotenv/config");
 const express = require("express");
 const logger = require("morgan");
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 
 // Local Modules
 const routes = require("./routes/index.js");
+const { PrismaClient } = require("@prisma/client");
 
 // Server Initialization
 const app = express();
@@ -14,6 +17,25 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("tiny"));
+
+// Session Setup
+app.use(
+  expressSession({
+    cookies: {
+      maxAge: 1000 * 60 * 60 * 24 * 3,
+      // secure: true, // Enable when deploy
+      httpOnly: true,
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 10 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
 
 // Routes
 app.use("/", routes);
